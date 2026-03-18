b7a5d737378137ab2284367d5a79e5272c968056
Zofia Lisowska-Petersen <zofiapetersenski@gmail.com>
2026-03-18 12:59:21 +0000

    minor corrections

--- models/core/fct_reviews.sql
+++ models/core/fct_reviews.sql
@@ -1,28 +1,35 @@
-{{ config(materialized='table') }}
+{{ config(materialized="table") }}
 
-with deduped as (
+with
+    deduped as (
 
-    select
-        review_id,
-        product_id,
-        user_id,
-        review_title,
-        review_content,
-        review_title_length,
-        review_content_length,
-        has_review_title,
-        has_review_content,
-        row_number() over (
-            partition by review_id, product_id, user_id
-            order by review_title, review_content
-        ) as rn
-    from {{ ref('stg_amazon_reviews') }}
-    where review_id is not null
+        select
+            review_id,
+            product_id,
+            user_id,
+            review_title,
+            review_content,
+            review_title_length,
+            review_content_length,
+            has_review_title,
+            has_review_content,
+            row_number() over (
+                partition by review_id, product_id, user_id
+                order by review_title, review_content
+            ) as rn
+        from {{ ref("stg_amazon_reviews") }}
+        where review_id is not null
 
-)
+    )
 
 select
-    md5(coalesce(review_id, '') || '|' || coalesce(product_id, '') || '|' || coalesce(user_id, '')) as review_sk,
+    md5(
+        coalesce(review_id, '')
+        || '|'
+        || coalesce(product_id, '')
+        || '|'
+        || coalesce(user_id, '')
+    ) as review_sk,
     review_id,
     product_id,
     user_id,
