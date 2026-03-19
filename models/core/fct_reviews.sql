178fc82cad28d09b73f31c9d79e16bf9d4549764
Zofia Lisowska-Petersen <zofiapetersenski@gmail.com>
2026-03-19 19:20:05 +0000

    Changes to fact table

--- models/core/fct_reviews.sql
+++ models/core/fct_reviews.sql
@@ -1,35 +1,30 @@
 {{ config(materialized="table") }}
 
-with
-    deduped as (
+with deduped as (
 
-        select
-            review_id,
-            product_id,
-            user_id,
-            review_title,
-            review_content,
-            review_title_length,
-            review_content_length,
-            has_review_title,
-            has_review_content,
-            row_number() over (
-                partition by review_id, product_id, user_id
-                order by review_title, review_content
-            ) as rn
-        from {{ ref("stg_amazon_reviews") }}
-        where review_id is not null
+    select
+        review_id,
+        product_id,
+        user_id,
+        review_title,
+        review_content,
+        review_title_length,
+        review_content_length,
+        has_review_title,
+        has_review_content,
 
-    )
+        row_number() over (
+            partition by review_id
+            order by review_content_length desc
+        ) as rn
+
+    from {{ ref("stg_amazon_reviews") }}
+    where review_id is not null
+
+)
 
 select
-    
-        coalesce(review_id, '')
-        || '|'
-        || coalesce(product_id, '')
-        || '|'
-        || coalesce(user_id, '')
-    as review_sk,
+    review_id as review_sk, 
     review_id,
     product_id,
     user_id,
@@ -39,5 +34,6 @@ select
     review_content_length,
     has_review_title,
     has_review_content
+
 from deduped
 where rn = 1
